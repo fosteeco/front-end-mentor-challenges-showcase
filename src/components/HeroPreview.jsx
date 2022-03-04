43ff429baa4c08384da9ca8e6510ca05fd427fcd
Christian Foster <fosterchris000@gmail.com>
2022-03-04 11:14:23 -0500

    Basic Hero preview implemented

--- src/components/HeroPreview.jsx
+++ src/components/HeroPreview.jsx
@@ -2,7 +2,17 @@ import React from "react";
 
 function HeroPreview(props) {
   const { challenge } = props;
-  return <div>{challenge.name}</div>;
+  const { name, repo, liveDemo, image } = challenge;
+  return (
+    <div>
+      {challenge.name}
+      <div class="flex items-center w-full mx-auto content-end">
+        <div class="browser-mockup flex">
+          <img src={image} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default HeroPreview;
